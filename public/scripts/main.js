562c0d91aa53647cf1dbcf92c73406d000804833
Dan <dan_oconnor@live.ca>
2019-04-04 17:24:45 -0400

    updates to HTML CSS JS

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,29 +1,12 @@
 "use strict";
 
-var userChoice = '';
-var compChoice = '';
-var compChoiceArray = ['rock', 'paper', 'scissors'];
-var userScore = 0;
-var compScore = 0;
-var draws = 0;
-var gamesPlayed = 0;
-var randomNumber;
 var app = {};
 
-app.genRandomNumber = function () {
-  randomNumber = Math.floor(Math.random() * 3);
-  return randomNumber;
-};
-
-app.trackGamesPlayed = function () {
-  gamesPlayed++;
-  $('#games_played').html(gamesPlayed.toString());
-};
-
-app.setCompChoice = function () {
-  app.genRandomNumber();
-  compChoice = compChoiceArray[randomNumber];
-  return compChoice;
+app.compChoice = function () {
+  var randomNumber = Math.floor(Math.random() * 3);
+  var compChoiceArray = ['rock', 'paper', 'scissors'];
+  var setCompChoice = compChoiceArray[randomNumber];
+  return setCompChoice;
 };
 
 app.closeModal = function () {
@@ -34,83 +17,53 @@ app.openModal = function () {
   $('.modal').removeClass('hidden').addClass('visible');
 };
 
-app.setUserChoiceRock = function () {
-  app.setCompChoice();
-  userChoice = 'rock';
+app.userScore = 0;
+app.compScore = 0;
+app.draws = 0;
+app.gamesPlayed = 0;
 
-  if (compChoice === 'paper') {
-    compScore++;
-    $('#comp_choice').html(compChoice);
-    $('#comp_score').html(compScore.toString());
+app.setUserChoice = function (compChoice, userChoice) {
+  console.log('compChoice: ', compChoice);
+  console.log('userChoice: ', userChoice);
+  $('#comp_choice').html("<img src=\"./public/assets/".concat(compChoice, ".png\" alt=\"cartoon hand indicating ").concat(compChoice, "\" />"));
+
+  if (compChoice === 'rock' && userChoice === 'scissors' || compChoice === 'paper' && userChoice === 'rock' || compChoice === 'scissors' && userChoice === 'paper') {
+    console.log('computy wins');
+    app.compScore++;
+    console.log('userScore: ', app.userScore);
+    console.log('compScore: ', app.compScore);
+    $('#comp_score').html(app.compScore.toString());
     $('#outcome').html("Computy goes ".concat(compChoice, " you lose!"));
-  } else if (compChoice === 'scissors') {
-    userScore++;
-    $('#comp_choice').html(compChoice);
-    $('#user_score').html(userScore.toString());
+  } else if (userChoice === 'rock' && compChoice === 'scissors' || userChoice === 'paper' && compChoice === 'rock' || userChoice === 'scissors' && compChoice === 'paper') {
+    console.log('user wins');
+    app.userScore++;
+    console.log('compScore: ', app.compScore);
+    console.log('userScore: ', app.userScore);
+    $('#user_score').html(app.userScore.toString());
     $('#outcome').html("Computy goes ".concat(compChoice, " you win!"));
   } else {
-    draws++;
-    $('#draws').html(draws.toString());
-    $('#comp_choice').html(compChoice);
+    console.log('draw');
+    app.draws++;
+    console.log('draws: ', app.draws);
+    $('#draws').html(app.draws.toString());
     $('#outcome').html("Computy goes ".concat(compChoice, " it's a draw!"));
   }
 
-  app.trackGamesPlayed();
+  app.gamesPlayed++;
+  $('#games_played').html(app.gamesPlayed.toString());
   app.openModal();
 };
 
-app.setUserChoicePaper = function () {
-  app.setCompChoice();
-  userChoice = 'paper';
-
-  if (compChoice === 'scissors') {
-    compScore++;
-    $('#comp_choice').html(compChoice);
-    $('#comp_score').html(compScore.toString());
-    $('#outcome').html("Computy goes ".concat(compChoice, " you lose!"));
-  } else if (compChoice === 'rock') {
-    userScore++;
-    $('#comp_choice').html(compChoice);
-    $('#user_score').html(userScore.toString());
-    $('#outcome').html("Computy goes ".concat(compChoice, " you win!"));
-  } else {
-    draws++;
-    $('#draws').html(draws.toString());
-    $('#comp_choice').html(compChoice);
-    $('#outcome').html("Computy goes ".concat(compChoice, " it's a draw!"));
-  }
-
-  app.trackGamesPlayed();
-};
-
-app.setUserChoiceScissors = function () {
-  app.setCompChoice();
-  userChoice = 'scissors';
-
-  if (compChoice === 'rock') {
-    compScore++;
-    $('#comp_choice').html(compChoice);
-    $('#comp_score').html(compScore.toString());
-    $('#outcome').html("Computy goes ".concat(compChoice, " you lose!"));
-  } else if (compChoice === 'paper') {
-    userScore++;
-    $('#comp_choice').html(compChoice);
-    $('#user_score').html(userScore.toString());
-    $('#outcome').html("Computy goes ".concat(compChoice, " you win!"));
-  } else {
-    draws++;
-    $('#draws').html(draws.toString());
-    $('#comp_choice').html(compChoice);
-    $('#outcome').html("Computy goes ".concat(compChoice, " it's a draw!"));
-  }
-
-  app.trackGamesPlayed();
-};
-
 app.init = function () {
-  $('#rock').on('click', app.setUserChoiceRock);
-  $('#paper').on('click', app.setUserChoicePaper);
-  $('#scissors').on('click', app.setUserChoiceScissors);
+  $('#rock').on('click', function () {
+    app.setUserChoice(app.compChoice(), this.id);
+  });
+  $('#paper').on('click', function () {
+    app.setUserChoice(app.compChoice(), this.id);
+  });
+  $('#scissors').on('click', function () {
+    app.setUserChoice(app.compChoice(), this.id);
+  });
   $('.play_again').on('click', app.closeModal);
 };
 
